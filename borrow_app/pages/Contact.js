abe87f84cb332d2dabb2cc86e0af98f7a64d569e
MessPh <math7064@gmail.com>
2023-03-11 14:38:04 +0700

    update

--- borrow_app/pages/Contact.js
+++ borrow_app/pages/Contact.js
@@ -1,14 +1,30 @@
 import UserNavbar from "@/components/UserNavbar";
 import Head from "next/head";
-
+import Link from "next/link";
+import styles from "@/styles/Contact.module.css"
 export default function Contact() {
     return (
         <>
             <Head>
                 <title>ติดต่อ | Contact</title>
             </Head>
+            
             <UserNavbar/>
-            <h1>This is Contactpage</h1>
-            </>
+            <h1>
+                <div className={styles.mes}>
+                    <label className = {styles.label} > Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu scelerisque elit. 
+                    <br/>Quisque interdum pellentesque rhoncus. Mauris tempor sollicitudin tincidunt. 
+                    <br/>Phasellus sodales ornare sagittis. Sed viverra neque dolor, quis tristique urna dignissim eu. 
+                    <br/>Phasellus euismod ex a odio dignissim hendrerit. Praesent sollicitudin ligula quis fermentum viverra. 
+                    <br/>Nam eget tristique leo, in gravida sem.Aenean tristique massa mauris, luctus 
+                    <br/>porta eros blandit at. Suspendisse quam orci, rhoncus eu purus a, 
+                    </label>
+                </div>
+                <img src="@/IMG/contact.jpg" alt="My Image" className={styles.image} />
+
+            </h1>
+        
+        </>
+            
     )
 };
--- borrow_app/pages/index.js
+++ borrow_app/pages/index.js
@@ -1,16 +1,56 @@
 import UserFooter from '@/components/UserFooter'
 import Link from "next/link"
 import styles from "@/styles/Home.module.css"
+import react from 'react'
+import { useState } from 'react';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Implement login logic here
+  };
+
   return (
     <>
-      <div className={styles.container}>
-        <h1 className={styles.title}> This is Loginpage</h1>
-        <button><Link href="/Contact">Go to Userpage</Link></button>
-      </div>
-      <UserFooter/>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Login</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="email" className={styles.label}>Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          className={styles.input}
+          required
+        />
+        <label htmlFor="password" className={styles.label}>Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          className={styles.input}
+          required
+        />
+        <button type="submit" className={styles.button}>Login</button>
+      </form>
       
+     
+    </div>
+    <UserFooter/>
     </>
-  )
+  );
 }
+
